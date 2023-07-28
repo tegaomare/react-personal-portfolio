f52f5908503340068918ddfee91f03666a143717
tegaomare <tegaomare@yahoo.com>
2023-07-28 19:11:45 -0400

    footer imported from components folder

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Works from "./components/Works";
-
+import Footer from "./components/Footer";
 function App() {
   return (
     <div>
