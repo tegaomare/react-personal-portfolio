25d168550d5f38592ddf63b2afa35b174f4e52ee
tegaomare <tegaomare@yahoo.com>
2023-07-27 23:53:29 -0400

    bg and text color changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,14 +7,14 @@ const Home = () => {
   return (
     <div
     name="home"
-    className="h-screen w-full bg-[#0a192f]"
+    className="h-screen w-full bg-[#A6A2A2]"
   >
     <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
       <div className="flex flex-col justify-center h-full">
-        <h2 className="text-4xl sm:text-7xl font-bold text-white">
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#11270B]">
          Software Engineer
         </h2>
-        <p className="text-gray-500 py-4 max-w-md">
+        <p className="text-black-500 py-4 max-w-md">
         I took a full stack web developer bootcamp from may 2023 - august 2023. I love to work on web application using technologies like
           React, Tailwind, Node.js, Express.js and Mongodb
         </p>
