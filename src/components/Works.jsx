64e25e905102181c26608abec56c9e3f41e7b5bd
tegaomare <tegaomare@yahoo.com>
2023-07-28 00:16:09 -0400

    bg & text color changed

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,10 +3,10 @@ import code from '../assets/code2.png';
 
 const Works = () => {
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+    <div name='work' className='w-full md:h-screen text-black-500 bg-[#A6A2A2]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8 w-full flex justify-center items-center flex-col'>
-          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-cyan-500'>
+          <p className='text-4xl font-bold inline border-b-4 text-black-500 border-[#11270B]'>
             Work
           </p>
           <p className='py-6 text-2xl'>Check out some of my most recent work</p>
