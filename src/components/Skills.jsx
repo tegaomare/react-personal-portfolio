7bef3d9316a12465d511c0ea0860c5ef22e79264
tegaomare <tegaomare@yahoo.com>
2023-07-28 00:45:49 -0400

    border color changed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,7 +6,7 @@ const Skills = () => {
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div className=' w-full flex justify-center items-center flex-col mb-7'>
-              <p className='text-4xl font-bold inline border-b-4 border-cyan-500 text-center '>Skills</p>
+              <p className='text-4xl font-bold inline border-b-4 border-[#11270B] text-center '>Skills</p>
               <p className='py-4 text-2xl'>I enjoy diving into and learning new things. Here's a list of technologies I've worked with</p>
           </div> 
           <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
