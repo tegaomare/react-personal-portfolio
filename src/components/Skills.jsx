c29011c740317f489e6ae9778b58e811d3870401
tegaomare <tegaomare@yahoo.com>
2023-07-28 09:30:56 -0400

    skills shadow color changed to [#178C38]

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,53 +10,53 @@ const Skills = () => {
               <p className='py-4 text-2xl'>I enjoy diving into and learning new things. Here's a list of technologies I've worked with</p>
           </div> 
           <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-              <div className='shadow-md shadow-[#11270B] hover:scale-110 duration-500'>
+              <div className='shadow-md shadow-[#178C38] hover:scale-110 duration-500'>
                   <p className='my-4'>HTML</p>
               </div>
-              <div className='shadow-md shadow-[#11270B] hover:scale-110 duration-500'>
+              <div className='shadow-md shadow-[#178C38] hover:scale-110 duration-500'>
                   <p className='my-4'>CSS</p>
               </div>
-              <div className='shadow-md shadow-[#11270B] hover:scale-110 duration-500'>
+              <div className='shadow-md shadow-[#178C38] hover:scale-110 duration-500'>
                   <p className='my-4'>JAVASCRIPT</p>
               </div>
-              <div className='shadow-md shadow-[#11270B] hover:scale-110 duration-500'>
+              <div className='shadow-md shadow-[#178C38] hover:scale-110 duration-500'>
                   <p className='my-4'>JAVA</p>
               </div>
-              <div className='shadow-md shadow-[#11270B] hover:scale-110 duration-500'>
+              <div className='shadow-md shadow-[#178C38] hover:scale-110 duration-500'>
                   <p className='my-4'>COMPUTER SCIENCE</p>
               </div>
 
-              <div className='shadow-md shadow-[#11270B] hover:scale-110 duration-500'>
+              <div className='shadow-md shadow-[#178C38] hover:scale-110 duration-500'>
                   <p className='my-4'>REACT</p>
               </div>
-              <div className='shadow-md shadow-[#11270B] hover:scale-110 duration-500'>
+              <div className='shadow-md shadow-[#178C38] hover:scale-110 duration-500'>
                   <p className='my-4'>GITHUB</p>
               </div>
-              <div className='shadow-md shadow-[#11270B] hover:scale-110 duration-500'>
+              <div className='shadow-md shadow-[#178C38] hover:scale-110 duration-500'>
                   <p className='my-4'>NODE JS</p>
               </div>
-              <div className='shadow-md shadow-[#11270B] hover:scale-110 duration-500'>
+              <div className='shadow-md shadow-[#178C38] hover:scale-110 duration-500'>
                   <p className='my-4'>MONGO DB</p>
               </div>
-              <div className='shadow-md shadow-[#11270B] hover:scale-110 duration-500'>
+              <div className='shadow-md shadow-[#178C38] hover:scale-110 duration-500'>
                   <p className='my-4'>MERN STACK</p>
               </div>
-              <div className='shadow-md shadow-[#11270B] hover:scale-110 duration-500'>
+              <div className='shadow-md shadow-[#178C38] hover:scale-110 duration-500'>
                   <p className='my-4'>MYSQL</p>
               </div>
-              <div className='shadow-md shadow-[#11270B] hover:scale-110 duration-500'>
+              <div className='shadow-md shadow-[#178C38] hover:scale-110 duration-500'>
                   <p className='my-4'>EXPRESS JS</p>
               </div>
-              <div className='shadow-md shadow-[#11270B] hover:scale-110 duration-500'>
+              <div className='shadow-md shadow-[#178C38] hover:scale-110 duration-500'>
                   <p className='my-4'>Mongodb</p>
               </div>
-              <div className='shadow-md shadow-[#11270B] hover:scale-110 duration-500'>
+              <div className='shadow-md shadow-[#178C38] hover:scale-110 duration-500'>
                   <p className='my-4'>HANDELBARS JS</p>
               </div>
-              <div className='shadow-md shadow-[#11270B] hover:scale-110 duration-500'>
+              <div className='shadow-md shadow-[#178C38] hover:scale-110 duration-500'>
                   <p className='my-4'>OOP</p>
               </div>
-              <div className='shadow-md shadow-[#11270B] hover:scale-110 duration-500'>
+              <div className='shadow-md shadow-[#178C38] hover:scale-110 duration-500'>
                   <p className='my-4'>SOFTWARE DEVELOPMENT</p>
               </div>
           </div>
