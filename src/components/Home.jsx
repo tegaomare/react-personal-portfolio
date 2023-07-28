d6f2e27a465ab76361879cda5143ffced04e4749
tegaomare <tegaomare@yahoo.com>
2023-07-27 22:53:27 -0400

    home text updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,12 +12,11 @@ const Home = () => {
     <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
       <div className="flex flex-col justify-center h-full">
         <h2 className="text-4xl sm:text-7xl font-bold text-white">
-          I'm a Full Stack Web Developer
+         Software Engineer
         </h2>
         <p className="text-gray-500 py-4 max-w-md">
-           I have 4 years of experience in graphics design and web development.
-          Currently, I love to work on web application using technologies like
-          React, Tailwind, Next.js and Mongodb.
+        I took a full stack web developer bootcamp from may 2023 - august 2023. I love to work on web application using technologies like
+          React, Tailwind, Node.js, Express.js and Mongodb
         </p>
         <div>
           <Link
