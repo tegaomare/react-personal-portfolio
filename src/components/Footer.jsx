3d6027e90371e5385742d7e26cb8b194f46af3f4
tegaomare <tegaomare@yahoo.com>
2023-07-28 20:04:24 -0400

    emoji width set to 25

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,23 @@
 import React from "react";
-import img1 from "../assets/angle.jpg"
 import img2 from "../assets/cool.png"
 export default function Footer() {
     return (
       <footer className="w-100 mt-auto bg-secondary p-4 bg-purple-900">
         <div className="container px-5 py-5 mx-auto text-center">
-          <span
-            className="emoji"
-            role="img"
-            aria-label="angle-face"
-            aria-hidden="false"
-          ></span>
-          <img src={img1} alt="Angle Face" />
-          <h4 className="pb-2">
-            Goodbye{" "}
-            <span
-              className="emoji"
-              role="img"
-              aria-label="tongue-face"
-              aria-hidden="false"
-            ></span>
-          </h4>
-          <img src={img2} alt="Tongue Face" />
+          <div className="message-container">
+            <h4 className="message">
+              Thank you.{" "}
+              <span
+                className="emoji"
+                role="img"
+                aria-label="tongue-face"
+                aria-hidden="false"
+              ></span>
+            </h4>
+          </div>
+          <div className="image-container">
+            <img src={img2} alt="Tongue Face" className="tongue-face-emoji" />
+          </div>
         </div>
       </footer>
     );
