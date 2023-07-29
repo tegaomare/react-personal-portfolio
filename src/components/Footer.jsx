05d9f9a5e4db009df20c66af0d10379321b0afe1
tegaomare <tegaomare@yahoo.com>
2023-07-28 20:09:42 -0400

    bg  text color changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import img2 from "../assets/cool.png"
 export default function Footer() {
     return (
-      <footer className="w-100 mt-auto bg-secondary p-4 bg-purple-900">
+      <footer className="w-100 mt-auto bg-secondary p-4 bg-[#11270B]">
         <div className="container px-5 py-5 mx-auto text-center">
           <div className="message-container">
-            <h4 className="message">
+            <h4 className="message text-gray-300">
               Thank you.{" "}
               <span
                 className="emoji"
