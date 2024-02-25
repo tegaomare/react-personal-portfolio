e8040bcbe43658a58d59373eb77a15c044c32918
tegaomare <tegaomare@yahoo.com>
2024-02-24 19:39:21 -0500

    updated linkedin link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -93,7 +93,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://www.linkedin.com/in/oghenetega-omarejedje-7749b027b/'
+              href='https://www.linkedin.com/in/tega-omarejedje-7749b027b/'
             >
               Linkedin <FaLinkedin size={30} />
             </a>
