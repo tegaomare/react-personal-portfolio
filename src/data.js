802462f56ebb780c831ccb6b57b7dd0ad511152b
tegaomare <tegaomare@yahoo.com>
2024-10-27 19:09:30 -0400

    renamed code to imageurl and also fixed typos in description

--- src/data.js
+++ src/data.js
@@ -1,62 +1,57 @@
-import code1 from "../src/assets/moviementor.png";
-import code2 from "../src/assets/f-f-t.png";
-import code3 from "../src/assets/R-P-S.png";
-import code4 from "../src/assets/code-quiz.png";
-import code5 from "../src/assets/css-snippet.png";
-import code6 from "../src/assets/tech-blog.png";
+import imageUrl1 from "../src/assets/moviementor.png";
+import imageUrl2 from "../src/assets/f-f-t.png";
+import imageUrl3 from "../src/assets/R-P-S.png";
+import imageUrl4 from "../src/assets/code-quiz.png";
+import imageUrl5 from "../src/assets/css-snippet.png";
+import imageUrl6 from "../src/assets/tech-blog.png";
 
 const applications = [
   {
-    code: code1,
+    imageUrl: imageUrl1,
     title: "MovieMentor",
     description:
-      "MovieMentor is a powerful movie app designed to enhance your movie-watching experience. ",
-
+      "MovieMentor is a powerful movie app designed to enhance your movie-watching experience.",
     codeLink: "https://project-211-88db9fc198e3.herokuapp.com/",
     GitHubLink: "https://github.com/tegaomare/moviementor",
   },
   {
-    code: code2,
-    title: "food-for-thought",
+    imageUrl: imageUrl2,
+    title: "Food-For-Thought",
     description:
-      "Food For Thought is a user-friendly webpage designed to help you find the perfect recipe based on your available ingredient.",
-
+      "Food For Thought is a user-friendly webpage designed to help you find the perfect recipe based on your available ingredients.",
     codeLink: "https://danielg808.github.io/food-for-thought/",
     GitHubLink: "https://github.com/DanielG808/food-for-thought",
   },
   {
-    code: code3,
+    imageUrl: imageUrl3,
     title: "Rock_Paper_Scissors",
     description:
-      "This is a web apllication game, againt a user and the computer called",
-
+      "This is a web application game, allowing users to play against the computer.",
     codeLink: "https://tegaomare.github.io/Rock_Paper_Scissors/",
     GitHubLink: "https://github.com/tegaomare/Rock_Paper_Scissors",
   },
   {
-    code: code4,
-    title: "Code-Qiuz",
+    imageUrl: imageUrl4,
+    title: "Code-Quiz",
     description: "A timed coding quiz with multiple-choice questions.",
-
-    codeLink: "https://tegaomare.github.io/Code-Qiuz/",
-    GitHubLink: "https://github.com/tegaomare/Code-Qiuz",
+    codeLink: "https://tegaomare.github.io/Code-Quiz/",
+    GitHubLink: "https://github.com/tegaomare/Code-Quiz",
   },
   {
-    code: code5,
-    title: "CSS-Snippet-Cheat--Sheet",
-    description: "a webpage that hold a collection of CSS snippets.",
-
+    imageUrl: imageUrl5,
+    title: "CSS-Snippet-Cheat-Sheet",
+    description: "A webpage that holds a collection of CSS snippets.",
     codeLink: "https://tegaomare.github.io/CSS-Snippet-Cheat--Sheet/",
     GitHubLink: "https://github.com/tegaomare/CSS-Snippet-Cheat--Sheet",
   },
   {
-    code: code6,
-    title: "tech-blog",
+    imageUrl: imageUrl6,
+    title: "Tech-Blog",
     description:
-      "This is a CMS-style blog site designed specifically for developers who want to publish articles, blog posts, and share their thoughts and opinions on various tech topics.",
-
+      "This is a CMS-style blog site for developers to publish articles, blog posts, and share their thoughts on tech topics.",
     codeLink: "https://cbctech-blog-3328d1c29db1.herokuapp.com/",
     GitHubLink: "https://github.com/tegaomare/tech-blog",
   },
 ];
+
 export default applications;
