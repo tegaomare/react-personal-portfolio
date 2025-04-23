014446c323d9c86e2c9da8bc9d127343e556406d
tegaomare <tegaomare@yahoo.com>
2025-04-23 12:51:10 -0400

    removed info about moviementor and tech blog because the heroku pages are down, then replaced moviementor with the gameofcells page

--- src/data.js
+++ src/data.js
@@ -1,18 +1,16 @@
-import imageUrl1 from "../src/assets/moviementor.png";
+import imageUrl1 from "../src/assets/gameofcells.png";
 import imageUrl2 from "../src/assets/f-f-t.png";
 import imageUrl3 from "../src/assets/R-P-S.png";
 import imageUrl4 from "../src/assets/code-quiz.png";
 import imageUrl5 from "../src/assets/css-snippet.png";
-import imageUrl6 from "../src/assets/tech-blog.png";
 
 const applications = [
   {
     imageUrl: imageUrl1,
-    title: "MovieMentor",
+    title: "GameOfCells",
     description:
-      "MovieMentor is a powerful movie app designed to enhance your movie-watching experience.",
-    codeLink: "https://project-211-88db9fc198e3.herokuapp.com/",
-    GitHubLink: "https://github.com/tegaomare/moviementor",
+      "A 2D real-time strategy game where players control a cell, manage resources, and evolve to survive in a hostile environment. Built with Java, libGDX.",
+    codeLink: "http://cs.potsdam.edu/Classes/405/BacklogBandits/",
   },
   {
     imageUrl: imageUrl2,
@@ -43,14 +41,6 @@ const applications = [
     description: "A webpage that holds a collection of CSS snippets.",
     codeLink: "https://tegaomare.github.io/CSS-Snippet-Cheat--Sheet/",
     GitHubLink: "https://github.com/tegaomare/CSS-Snippet-Cheat--Sheet",
-  },
-  {
-    imageUrl: imageUrl6,
-    title: "Tech-Blog",
-    description:
-      "This is a CMS-style blog site for developers to publish articles, blog posts, and share their thoughts on tech topics.",
-    codeLink: "https://cbctech-blog-3328d1c29db1.herokuapp.com/",
-    GitHubLink: "https://github.com/tegaomare/tech-blog",
   },
 ];
 
